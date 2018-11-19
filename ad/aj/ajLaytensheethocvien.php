fc9a42f34e3d1d6fa3b3c171a96f2f06fc622d72
lythanhngodev <lythanhngodev@gmail.com>
2018-11-19 22:39:51 +0700

    .

--- ad/aj/ajLaytensheethocvien.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "../ec/PHPExcel.php";
-$file = $_FILES['file']['tmp_name'];
-$objReader = PHPExcel_IOFactory::createReaderForFile($file);
-$listWorkSheets = $objReader->listWorksheetNames($file);
-echo json_encode($listWorkSheets);
-?>
