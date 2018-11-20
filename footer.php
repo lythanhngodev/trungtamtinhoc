ec62cd45caf696fdb8919e8b5d16356576cfa1c7
lythanhngodev <lythanhngodev@gmail.com>
2018-11-20 22:32:08 +0700

    update

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <footer class="main-footer">
-    <div class="pull-right text-sm">
+    <div class="pull-right text">
         <br>
         <strong>Phiên bản</strong> 20.11.2018<br>
         <strong>© 2018 <a href="https://fb.com/ly.ngothanh" target="_blank">Ngô Thanh Lý</a></strong>
     </div>
-    <span class="text-sm text-blue">
+    <span class="text-sm text-dark">
         <strong>TRƯỜNG ĐẠI HỌC SƯ PHẠM KỸ THUẬT VĨNH LONG</strong><br>
         <strong>Trung tâm Đào tạo Tin học</strong><br>
         Địa chỉ: 73 Nguyễn Huệ, Phường 2, TP. Vĩnh Long, Vĩnh Long<br>
-        Email: cit@vlute.edu.vn
+        Điện thoại: +842703 828320&ensp;Email: cit@vlute.edu.vn
     </span>
 </footer>
--- index.php
+++ index.php
@@ -149,14 +149,7 @@ ob_start("sanitize_output");
   <!-- /.content-wrapper -->
 
   <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      Phiên bản: 20.11.18
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2018 <a href="#">Ngô Thanh Lý</a> </strong>(Faculty of Information Technology 2014)
-  </footer>
+<?php require_once 'footer.php'; ?>
   <!-- Add the sidebar's background. This div must be placed
   immediately after the control sidebar -->
   <div class="control-sidebar-bg"></div>
